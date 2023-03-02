9269e178ef0cd2046a4476ac31e0523c20213a0a
l524l <perschin998@gmail.com>
2023-03-02 18:27:53 +0300

    Добалено отображение даты в расписание на неделю. Доработан функцианал связаныый с отображением расписания на завтра и понедельник

--- app/src/main/java/site/l524l/diary/DayFragment.java
+++ app/src/main/java/site/l524l/diary/DayFragment.java
@@ -11,7 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.time.LocalDateTime;
 import java.time.format.TextStyle;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -21,7 +23,9 @@ import site.l524l.diary.lesson.Lesson;
 public class DayFragment extends Fragment {
 
     private static final String DAY_OF_WEAK = "day_of_weak";
+    private static final String LOCAL_DATE_TIME = "local_date_time";
     private Day day;
+    private LocalDateTime localDateTime;
 
 
     public static DayFragment newInstance(Day param1) {
@@ -42,6 +46,7 @@ public class DayFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             day = (Day) getArguments().getSerializable(DAY_OF_WEAK);
+            localDateTime = (LocalDateTime) getArguments().getSerializable(LOCAL_DATE_TIME);
         }
     }
 
@@ -54,12 +59,13 @@ public class DayFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         TextView z = view.findViewById(R.id.textView11);
+        TextView date = view.findViewById(R.id.datetime);
         Group labelsGroup = view.findViewById(R.id.leasons_labels);
         int[] ids = labelsGroup.getReferencedIds();
         List<Lesson> lessons = day.getLessons();
 
         z.setText(day.getDayOfWeak().getDisplayName(TextStyle.FULL, Locale.forLanguageTag("ru")).toUpperCase(Locale.ROOT));
-
+        date.setText(String.format("%s %s", localDateTime.getDayOfMonth(),localDateTime.getMonth().getDisplayName(TextStyle.FULL, Locale.forLanguageTag("ru"))));
         for (int i = 0; i < lessons.size(); i++) {
             TextView textView = view.findViewById(ids[i]);
             textView.setText(lessons.get(i).getName());
